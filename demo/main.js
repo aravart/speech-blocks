75a40b2d3fb683b6754c7b13960c4af28025af2d
Evan Hernandez <evanfredhernandez@gmail.com>
2016-10-01 15:53:53 -0500

    Allow manual adding of blocks in the demo.

--- demo/main.js
+++ demo/main.js
@@ -1,5 +1,5 @@
 /** 
- * @fileoverview This is just a bunch of testing/demo nonsense. 
+ * @fileoverview Initialize the demo. 
  * @author ehernandez4@wisc.edu (Evan Hernandez)
  */
 'use strict';
@@ -11,16 +11,41 @@ var controller = new SpeechBlocks.Controller('blocklyDiv',
 var setErrorMessage = function(content) {
   document.getElementById('errorMessage').textContent = content;
 }
+var resetErrorMessage = function() { setErrorMessage(''); };
 
-// Create handlers for adding new blocks.
+// Provide easy access to input elements.
 var newBlockIdInput = document.getElementById('newBlockId');
 var newBlockTypeInput = document.getElementById('newBlockType');
 var newBlockButton = document.getElementById('createNewBlock');
 
+var moveBlockIdInput = document.getElementById('moveWhat');
+var moveBlockLocationInput = document.getElementById('moveWhere');
+var moveBlockButton = document.getElementById('moveBlock');
+
+var removeBlockIdInput = document.getElementById('removeWhat');
+var removeBlockButton = document.getElementById('removeBlock');
+
+// Provide easy way to update IDs.
+var addId = function(id) {
+  var newIdOption = document.createElement('option');
+  newIdOption.text = newIdOption.value = id;
+  moveBlockIdInput.add(newIdOption);
+  removeBlockIdInput.add(newIdOption.cloneNode(true));
+};
+
+var removeId = function(id) {
+  // TODO(evanfredhernandez): Implement this function.
+}
+
+// Create handlers for adding new blocks.
 newBlockButton.addEventListener('click', function() {
-  if (!newBlockIdInput.getAttribute('value')) {
+  resetErrorMessage();
+  var id = newBlockIdInput.value;
+  if (!id) {
     setErrorMessage('Please provide an ID!');
     return;
   }
-  errorM
+  var type = newBlockTypeInput.options[newBlockTypeInput.selectedIndex].value;
+  controller.addBlock(type, id, new SpeechBlocks.Translation(0, 0));
+  addId(id);
 });
