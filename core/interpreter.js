7f4045f574ffc703e3754bcf98c6e15edf5b37a2
David Liang <davidliangx27@gmail.com>
2016-10-03 14:25:50 -0500

    Move implemented, won't work until controller bug fixed

--- core/interpreter.js
+++ core/interpreter.js
@@ -1,8 +1,8 @@
 /**
- * @fileoverview Interprets a given command and calls workspace controller functions.
- * @author pandori@wisc.edu (Sahib Pandori)
- * @author david.liang@wisc.edu (David Liang)
- */
+* @fileoverview Interprets a given command and calls workspace controller functions.
+* @author pandori@wisc.edu (Sahib Pandori)
+* @author david.liang@wisc.edu (David Liang)
+*/
 
 'use strict';
 
@@ -13,9 +13,9 @@ goog.require('goog.structs.Map');
 var blockTypeMap = new goog.structs.Map();
 
 /**
- * Delete a specified block.
- * @param command Command object from parser.
- */
+* Delete a specified block.
+* @param command Command object from parser.
+*/
 SpeechBlocks.Interpreter = function(controller) {
   this.controller = controller;
   this.id = 1;
@@ -23,9 +23,9 @@ SpeechBlocks.Interpreter = function(controller) {
 }
 
 /**
- * @TODO Allow for file input.
- * Creates the blockTypeMap.
- */
+* @TODO Allow for file input.
+* Creates the blockTypeMap.
+*/
 SpeechBlocks.Interpreter.createBlockTypeMap = function() {
   blockTypeMap.set('if','controls_if');
   blockTypeMap.set('comparison','logic_compare');
@@ -39,9 +39,9 @@ SpeechBlocks.Interpreter.createBlockTypeMap = function() {
 }
 
 /**
- * Interprets a given command by calling the corresponding action function.
- * @param {Object=} command Command object from parser.
- */
+* Interprets a given command by calling the corresponding action function.
+* @param {Object=} command Command object from parser.
+*/
 SpeechBlocks.Interpreter.prototype.interpret = function(command) {
   if (command.action == "run") {
     this.run(command);
@@ -57,9 +57,9 @@ SpeechBlocks.Interpreter.prototype.interpret = function(command) {
 };
 
 /**
- * Compiles the code into JavaScript and runs it.
- * @param {Object=} command Object with command specifics from the parser.
- */
+* Compiles the code into JavaScript and runs it.
+* @param {Object=} command Object with command specifics from the parser.
+*/
 SpeechBlocks.Interpreter.prototype.run = function(command) {
   Blockly.JavaScript.addReservedWords('code');
   var code = Blockly.JavaScript.workspaceToCode(this.controller.workspace);
@@ -71,35 +71,54 @@ SpeechBlocks.Interpreter.prototype.run = function(command) {
 };
 
 /**
- * Adds a specified block.
- * @param {Object=} command Command object from parser.
- */
+* Adds a specified block.
+* @param {Object=} command Command object from parser.
+*/
 SpeechBlocks.Interpreter.prototype.addBlock = function(command) {
   controller.addBlock(blockTypeMap.get(command.type), this.id++, new SpeechBlocks.Translation(0,0));
 };
 
 /**
- * @TODO implement with proper where
- * Moves a specified block.
- * @param {Object=} command Command object from parser.
- */
+* @TODO implement with proper where
+* Moves a specified block.
+* @param {Object=} command Command object from parser.
+*/
 SpeechBlocks.Interpreter.prototype.moveBlock = function(command) {
-  controller.moveBlock(command.block.number, command.where);
+  console.log(command.where);
+  if (this.doesBlockExist(command.block)) {
+    if (command.where = "trash")
+    this.deleteBlock(command.block);
+    else
+    controller.moveBlock(command.block.number, command.where);
+  }
+  else {
+    console.log("Block to be moved cannot be found");
+  }
 };
 
 /**
- * @TODO implement
- * Modifies a specified block.
- * @param {Object=} command Command object from parser.
- */
+* @TODO implement
+* Modifies a specified block.
+* @param {Object=} command Command object from parser.
+*/
 SpeechBlocks.Interpreter.prototype.modifyBlock = function(command) {
 
 };
 
 /**
- * Delete a specified block.
- * @param {Object=} command Command object from parser.
- */
+* Delete a specified block.
+* @param {Object=} command Command object from parser.
+*/
 SpeechBlocks.Interpreter.prototype.deleteBlock = function(command) {
-  controller.removeBlock(command.block.number);
+  controller.removeBlock(command.block);
 };
+
+SpeechBlocks.Interpreter.prototype.doesBlockExist = function(blockRequestID) {
+  var found = false;
+  this.controller.getAllBlockIds().forEach(function(id) {
+    if (blockRequestID == id) {
+      found = true;
+    }
+  });
+  return found;
+}
