7c62c034d1f821cb3fa54c57cb1aa484daba7398
David Liang <davidliangx27@gmail.com>
2016-10-04 23:31:02 -0500

    Added functionality for move

--- core/interpreter.js
+++ core/interpreter.js
@@ -20,21 +20,21 @@ goog.require('goog.structs.Map');
 * @param {!SpeechBlocks.Controller} controller_.The workspace controller_.
 */
 SpeechBlocks.Interpreter = function(controller) {
-	/** @private @const */
-	this.controller_ = controller;
-	/** @private */
-	this.id_ = 1;
-	/** @private */
-	this.blockTypeMap_ = new goog.structs.Map();
-	this.blockTypeMap_.set('if','controls_if');
-	this.blockTypeMap_.set('comparison','logic_compare');
-	this.blockTypeMap_.set('repeat','controls_repeat_ext');
-	this.blockTypeMap_.set('number','math_number');
-	this.blockTypeMap_.set('arithmetic','math_arithmetic');
-	this.blockTypeMap_.set('text','text');
-	this.blockTypeMap_.set('print','text_print');
-	this.blockTypeMap_.set('set','variables_set');
-	this.blockTypeMap_.set('variable','variables_get');
+  /** @private @const */
+  this.controller_ = controller;
+  /** @private */
+  this.id_ = 1;
+  /** @private */
+  this.blockTypeMap_ = new goog.structs.Map();
+  this.blockTypeMap_.set('if','controls_if');
+  this.blockTypeMap_.set('comparison','logic_compare');
+  this.blockTypeMap_.set('repeat','controls_repeat_ext');
+  this.blockTypeMap_.set('number','math_number');
+  this.blockTypeMap_.set('arithmetic','math_arithmetic');
+  this.blockTypeMap_.set('text','text');
+  this.blockTypeMap_.set('print','text_print');
+  this.blockTypeMap_.set('set','variables_set');
+  this.blockTypeMap_.set('variable','variables_get');
 }
 
 /**
@@ -42,14 +42,14 @@ SpeechBlocks.Interpreter = function(controller) {
 * @param {Object=} command Command object from parser.
 */
 SpeechBlocks.Interpreter.prototype.interpret = function(command) {
-	switch (command.action) {
-		case 'run': this.run(command); break;
-		case 'add': this.addBlock(command); break;
-		case 'move': this.moveBlock(command); break;
-		case 'modify': this.modifyBlock(command); break;
-		case 'delete': this.deleteBlock(command); break;
-		default: console.log('Unsupported command');
-	}
+  switch (command.action) {
+    case 'run': this.run(command); break;
+    case 'add': this.addBlock(command); break;
+    case 'move': this.moveBlock(command); break;
+    case 'modify': this.modifyBlock(command); break;
+    case 'delete': this.deleteBlock(command); break;
+    default: console.log('Unsupported command');
+  }
 };
 
 /**
@@ -57,9 +57,9 @@ SpeechBlocks.Interpreter.prototype.interpret = function(command) {
 * @param {Object=} command Object with command specifics from the parser.
 */
 SpeechBlocks.Interpreter.prototype.run = function(command) {
-	Blockly.JavaScript.addReservedWords('code');
-	var code = Blockly.JavaScript.workspaceToCode(this.controller_.workspace);
-	eval(code);
+  Blockly.JavaScript.addReservedWords('code');
+  var code = Blockly.JavaScript.workspaceToCode(this.controller_.workspace);
+  eval(code);
 };
 
 /**
@@ -67,8 +67,8 @@ SpeechBlocks.Interpreter.prototype.run = function(command) {
 * @param {Object=} command Command object from parser.
 */
 SpeechBlocks.Interpreter.prototype.addBlock = function(command) {
-	var type = this.blockTypeMap_.get(command.type);
-	if (type != null) { this.controller_.addBlock(type, (this.id_++).toString(), new SpeechBlocks.Translation(0,0)); }
+  var type = this.blockTypeMap_.get(command.type);
+  if (type != null) { this.controller_.addBlock(type, (this.id_++).toString(), new SpeechBlocks.Translation(0,0)); }
 };
 
 /**
@@ -77,37 +77,37 @@ SpeechBlocks.Interpreter.prototype.addBlock = function(command) {
 * @param {Object=} command Command object from parser.
 */
 SpeechBlocks.Interpreter.prototype.moveBlock = function(command) {
-	// ensure these fields are strings, so validity checking works properly
-	command.block = command.block.toString();
-	command.where.block = command.where.block.toString();
-	if (this.isBlockIdValid(command.block)) {
-		if (command.where == 'trash') {
-			this.deleteBlock(command.block);
-		} else {
-			if (this.isBlockIdValid(command.where.block)) {
-				switch (command.where.position) {
-					// modify these so the 'where' field is right
-					case 'after': this.controller_.moveBlock(command.block, new SpeechBlocks.Successor(command.where.block)); break;
-					case 'before': this.controller_.moveBlock(command.block, new SpeechBlocks.Predecessor(command.where.block)); break;
-					case 'lhs': case 'rhs': case 'to the right of':
-					var inputList_ = this.controller_.getBlockValueInputs(command.where.block);
-					if (inputList_.length < 1) { console.log('NO VALUE INPUTS FOR SPECIFIED BLOCK'); }
-					else if (command.where.position == 'rhs' || command.where.position == 'to the right of') { this.controller_.moveBlock(command.block, new SpeechBlocks.ValueInput(command.where.block, inputList_[inputList_.length-1]));	}
-					else { this.controller_.moveBlock(command.block, new SpeechBlocks.ValueInput(command.where.block, inputList_[0])); }
-					case 'inside':
-					var statementList_ = this.controller_.getBlockStatementInputs(command.where.block);
-					if (statementList_.length < 1) { console.log('NO STATEMENT INPUTS FOR SPECIFIED BLOCK'); }
-					else { this.controller_.moveBlock(command.block, new SpeechBlocks.StatementInput(command.where.block, statementList_[statementList_.length-1])); }
-					break;
-					default:
-					console.log('Unsupported position');
-				}
-			}
-		}
-	}
-	else {
-		console.log('Invalid block Id');
-	}
+  // ensure these fields are strings, so validity checking works properly
+  command.block = command.block.toString();
+  command.where.block = command.where.block.toString();
+  if (this.isBlockIdValid(command.block)) {
+    if (command.where == 'trash') {
+      this.deleteBlock(command.block);
+    } else {
+      if (this.isBlockIdValid(command.where.block)) {
+        switch (command.where.position) {
+          // modify these so the 'where' field is right
+          case 'after': this.controller_.moveBlock(command.block, new SpeechBlocks.Successor(command.where.block)); break;
+          case 'before': this.controller_.moveBlock(command.block, new SpeechBlocks.Predecessor(command.where.block)); break;
+          case 'lhs': case 'rhs': case 'to the right of':
+          var inputList_ = this.controller_.getBlockValueInputs(command.where.block);
+          if (inputList_.length < 1) { console.log('NO VALUE INPUTS FOR SPECIFIED BLOCK'); }
+          else if (command.where.position == 'rhs' || command.where.position == 'to the right of') { this.controller_.moveBlock(command.block, new SpeechBlocks.ValueInput(command.where.block, inputList_[inputList_.length-1]));	}
+          else { this.controller_.moveBlock(command.block, new SpeechBlocks.ValueInput(command.where.block, inputList_[0])); }
+          case 'inside':
+          var statementList_ = this.controller_.getBlockStatementInputs(command.where.block);
+          if (statementList_.length < 1) { console.log('NO STATEMENT INPUTS FOR SPECIFIED BLOCK'); }
+          else { this.controller_.moveBlock(command.block, new SpeechBlocks.StatementInput(command.where.block, statementList_[statementList_.length-1])); }
+          break;
+          default:
+          console.log('Unsupported position');
+        }
+      }
+    }
+  }
+  else {
+    console.log('Invalid block Id');
+  }
 };
 
 /**
@@ -116,36 +116,36 @@ SpeechBlocks.Interpreter.prototype.moveBlock = function(command) {
 * @param {Object=} command Command object from parser.
 */
 SpeechBlocks.Interpreter.prototype.modifyBlock = function(command) {
-	if (this.isBlockIdValid(command.block)) {
-		switch(command.property) {
-			case 'number':
-			command.value = Number(command.value);
-			this.controller.setBlockField(command.block, fieldName, command.value);
-			break;
-			case 'text':
+  if (this.isBlockIdValid(command.block)) {
+    switch(command.property) {
+      case 'number':
+      command.value = Number(command.value);
+      this.controller.setBlockField(command.block, fieldName, command.value);
+      break;
+      case 'text':
 
-			this.controller.setBlockField(command.block, fieldName, command.value);
-			break;
-			case 'comparison':
+      this.controller.setBlockField(command.block, fieldName, command.value);
+      break;
+      case 'comparison':
 
-			this.controller.setBlockField(command.block, fieldName, command.value);
-			break;
-			case 'operation':
+      this.controller.setBlockField(command.block, fieldName, command.value);
+      break;
+      case 'operation':
 
-			this.controller.setBlockField(command.block, fieldName, command.value);
-			break;
-			case 'name':
+      this.controller.setBlockField(command.block, fieldName, command.value);
+      break;
+      case 'name':
 
-			this.controller.setBlockField(command.block, fieldName, command.value);
-			break;
-			default:
-			console.log('Unsupported property');
-		}
-		console.log('MODIFY NOT YET SUPPORTED');
-	}
-	else {
-		console.log('Invalid block Id');
-	}
+      this.controller.setBlockField(command.block, fieldName, command.value);
+      break;
+      default:
+      console.log('Unsupported property');
+    }
+    console.log('MODIFY NOT YET SUPPORTED');
+  }
+  else {
+    console.log('Invalid block Id');
+  }
 };
 
 /**
@@ -153,8 +153,8 @@ SpeechBlocks.Interpreter.prototype.modifyBlock = function(command) {
 * @param {Object=} command Command object from parser.
 */
 SpeechBlocks.Interpreter.prototype.deleteBlock = function(blockId) {
-	if (this.isBlockIdValid(blockId)) { this.controller_.removeBlock(blockId); }
-	else { console.log('Invalid block Id'); }
+  if (this.isBlockIdValid(blockId)) { this.controller_.removeBlock(blockId); }
+  else { console.log('Invalid block Id'); }
 };
 
 /**
@@ -162,5 +162,5 @@ SpeechBlocks.Interpreter.prototype.deleteBlock = function(blockId) {
 * @param {!String=} blockId as string.
 */
 SpeechBlocks.Interpreter.prototype.isBlockIdValid = function(blockRequestId) {
-	return this.controller_.getAllBlockIds().contains(blockRequestId);
+  return this.controller_.getAllBlockIds().contains(blockRequestId);
 }
