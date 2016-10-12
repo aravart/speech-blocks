1fcb5b0a12fbf6b39519b60766c8c92ad084f4f4
David Liang <davidliangx27@gmail.com>
2016-10-11 22:35:19 -0500

    Better interpreter unit testing

--- test/interpreter_test.js
+++ test/interpreter_test.js
@@ -1,31 +1,21 @@
-goog.require('SpeechBlocks.Workspace')
-goog.require('SpeechBlocks.Controller');
 goog.require('SpeechBlocks.Interpreter');
 
 'use strict';
-var workspace = new Blockly.Workspace();
-var controller = new SpeechBlocks.Controller(workspace);
-var interpreter = new SpeechBlocks.Interpreter(controller);
 
+QUnit.test('Add test', function(assert) {
 
-QUnit.test('Add block test', function(assert) {
-    var pre_exist = interpreter.controller_.workspace_.getBlockById(1);
+    var controller = {
+        addBlock: function(type, blockId, where) {
+            assert.ok(where instanceof SpeechBlocks.Translation,'pass: where');
+            assert.ok(type == 'controls_if', 'pass: type');
+        }
+    }
+
     var command = {
         'action': 'add',
         'type': 'if'
     }
+
+    var interpreter = new SpeechBlocks.Interpreter(controller);
     interpreter.interpret(command);
-    var post_exist = interpreter.controller_.workspace_.getBlockById(1);
-    assert.ok(!pre_exist && post_exist, 'Block 1 successfully added to workspace');
-
-    var pre_exist = interpreter.controller_.workspace_.getBlockById(2);
-    var command = {
-        'action': 'add',
-        'type': 'repeat'
-    }
-    interpreter.interpret(command);
-    var post_exist = interpreter.controller_.workspace_.getBlockById(2);
-    assert.ok(!pre_exist && post_exist, 'Block 2 successfully added to workspace');
-
-
 });
